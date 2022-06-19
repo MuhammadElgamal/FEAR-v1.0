1f7d57a500a2bb585cc394b9f0445b90027887af
Muhammad Elgamal <elgamalmuhammad@gmail.com>
2022-06-19 20:40:21 +0200

    removing replicated files

--- example5.m
+++ example5.m
@@ -1,48 +1,44 @@
-clc; clear all; close all;
-%% Example 1 Evaluation
-% This example runs on the paper of 
-% Lin, Yi-Kuei, Cheng-Ta Yeh, and Cheng-Fu Huang. "Reliability evaluation of a 
-% stochastic-flow distribution network  with delivery spoilage." 
-% Computers & Industrial Engineering 66.2 (2013): 352-359.
-% We are concerned about the flow from Kaosiung to Hangzhou city 
-% through arcs of 1 , 2, 9, 10 which we reorder as 1, 2, 3, 4 respectively and 
-% rename the source as 5 and Shanghai as 6, Wenzhou as 7 and Hangzhou as 8
-% hi babe
-
-
-source=5; target=8; 
-directed=true; 
+close all; clc; clear all;
+%% Description
+% In this section we estimate a time dependent reliability of a multistate
+% system but in a comprehensive way
+%% Defining the network geometry
+source=7; target=10;
+directed=true;
 nodes_neglected=true;
 terminals_excluded=true;
- 
-source_nodes=[5 5 6 7];
-target_nodes=[6 7 8 8];
+
+source_nodes=[7 8 8 9 7 9 ];
+target_nodes=[8 10 9 8 9 10];
 net=reliability_net(source, target, directed, nodes_neglected, ...
-        terminals_excluded, source_nodes, target_nodes);
- 
-%_______________ Entering Maximal Capacity for each element _______________
-CP=cell(1,4);           
-CP{1}=[0.002 0.004 0.005 0.006 0.007 0.01 0.013 0.033 0.92]; 
-CP{2}=[0.006 0.007 0.007 0.011 0.013 0.016 0.016 0.024 0.9];       
-CP{3}=[0.01 0.01 0.05 0.013 0.013 0.018 0.886];           
-CP{4}=[0.008 0.008 0.01 0.011 0.013 0.03 0.039 0.881];
+    terminals_excluded, source_nodes, target_nodes);
+sort_output = true;
+net.shortest_paths (sort_output);
+net.flow_constraints={'flow=demand', 'maximal capacity constraint'};
+%% Defining dynamic demand
+% inputs d, t
+% outputs R
+net.time = linspace(0, 160, 10);
+d = 40*sin(2*pi*net.time/400).^2;
+net.demand_in_time = round(d);           % to ensure integer-valued demand
+w = [4 3 2 3 2];            % maximal component per each arc
+k = [10 15 25 15 20];       % maximal capacity per each component
+alpha = [300 180 200 350 250];
+beta = [1 1 0.8 1.1 1];
+%--------------- Reordering according to our paper original order
+order_vec = [4 5 3  3 1 2];
+w = w(order_vec);
+k = k(order_vec);
 
-demand=randi([1 10], 1, max(cellfun(@(h) length(h), CP)));
-demand=demand/sum(demand);
-
-take_capacity (net, CP, demand);
-shortest_paths (net, true);
-
-net.flow_constraints={'flow=demand', 'less than Lj', 'maximal capacity constraint'};
-evaluate_reliability(net, true, [], [], false);
-%% Displaying network data
-disp(net);
-%% Plotting the Reliability curve
-x = net.reliability.demand;
-y = net.reliability.rel;
-plot(x,y, "LineWidth",3, "Marker","x","MarkerSize",10)
-grid on;
-xlabel("Demand");
-ylabel("Reliability");
-figure;
-plot(net);
+alpha = alpha(order_vec);
+beta = beta(order_vec);
+net.k = k;
+%---------------- Defining reliability functions ------------------
+% building arc reliability matrix
+dist = repmat("weibull", 1,6);
+parameters = cell(1, 6);
+for i=1:6
+    parameters{i} = [alpha(i), beta(i)];
+end
+net.reliability_dependent_time (w, dist, parameters);
+net.plot_time_reliablity();
